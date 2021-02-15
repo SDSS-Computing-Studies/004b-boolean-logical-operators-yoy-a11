a9ce4f00ca6aab6c9403a2e5b498626cfd3ccb47
yoyo-a11 <yoyol264@deltalearns.ca>
2021-02-15 13:36:55 -0800

    Update problem1.py

--- problem1.py
+++ problem1.py
@@ -19,9 +19,8 @@ Enter a number: 48
 """
 
 #! python3
-number = int(input)
-if number % 6 == 0:
-    if number % 8 != 0:
-        print(str(number) + " is frue")
+number = int(input())
+if number % 6 == 0 and number % 8 != 0:
+    print(str(number) + " is frue")
 else:
     print(str(number) + " is not frue")
